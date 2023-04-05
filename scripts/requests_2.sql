7cb04c87849d1e66b4b8ae5114361b2df22d3ded
potapov daniil <potapov.da@phystech.edu>
2023-04-05 13:29:03 +0000

    Revert "Add files"
    This reverts commit 72c80e4741c27df123f87c5698ec09e67a33b548

--- scripts/requests_2.sql
+++
@@ -1,6 +0,0 @@
-SELECT rating, COUNT(*) FROM Medialibrary."Movie Library" GROUP BY rating HAVING AVG(number_of_raitings) > 20000;
-SELECT * FROM Medialibrary."Movie Library" ORDER BY rating DESC;
-SELECT country, AVG(duration) OVER(PARTITION BY country) FROM Medialibrary."Movie Description";
-SELECT country, COUNT(*) OVER(PARTITION BY country) FROM Medialibrary.Actors;
-SELECT name, ROW_NUMBER() OVER(ORDER BY country) AS row_num FROM Medialibrary."Movie Description";
-SELECT *, LAG(duration, 1, 0) OVER(ORDER BY country) AS prev_date FROM Medialibrary."Movie Description";
